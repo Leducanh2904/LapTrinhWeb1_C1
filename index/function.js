3fa96e70deb2cfcf5a6866025eee31a31de5a790
Leducanh2904 <ldanhsguk22@gmail.com>
2023-12-08 23:33:29 +0700

    Chinh sua lan 1

--- index/function.js
+++ index/function.js
@@ -6,3 +6,11 @@ function changAccountName(){
         document.getElementById("taikhoan").innerHTML = "LÊ ĐỨC ANH";
     }
 }
+function changeAddress(){
+    if(document.pay.rd1.value == "Chon"){
+        document.getElementById("address").value="273 An Dương Vương, phường 3 Quận 5, TP. Hồ Chí Minh";
+    }
+    if(document.pay.rd1.value == "Nhap"){
+        document.getElementById("address").value=null;
+    }
+}
